d7e2f2238ab4ac4a7e8252fcec481d72087c74cb
Tara Zadlyka <tara02zadlyka@gmail.com>
2021-10-20 08:14:20 +0700

    perbaiki tampilan

--- app/Views/content/dosen/index.php
+++ app/Views/content/dosen/index.php
@@ -1,19 +1,50 @@
-<?= $this->extend('layout/dosen') ;?>
-<?= $this->section('content') ;?>
+<?= $this->extend('layout/dosen'); ?>
+<?= $this->section('content'); ?>
+<section id="home" class="home">
+    <div class="container p-5">
+        <div class="row">
+            <div class="col-12 pb-5">
+                <div id="tanggal"></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-8">
+                <h1 class="fw-bold">Ayo Bimbingan !!</h1>
+                <p>Jadwalkan bimbinganmu dengan BTA</p>
+                <span class="baru">
+                    <a href="#login" id="login" class="btn btn-warning fw-bold text-white me-2 px-4">Mulai</a>
+                </span>
+            </div>
+            <div class="col-sm-0 col-md-4">
+                <img src="<?= base_url('assets/img/schedule.svg') ?>" class="float-end" width="304" height="236" alt="login">
+            </div>
+        </div>
+    </div>
+</section>
 
-<div>
-    <h2>Jumlah Pengajuan Bimbingan Yang Masuk</h2>
-    <h3><?=count($pengajuan); ?></h3>
-</div>
+<section id="pengajuan" class="pengajuan">
+    <div class="container mt-3">
+        <div class="row">
+            <div class="bb col-md my-2 me-2 bg-warning">
+                <div class="box py-5 mt-1 text-center text-white">
+                    <p>Jumlah Pengajuan yang Masuk</p>
+                    <span class="display-6 fw-bold"><?= count($pengajuan); ?></span>
+                </div>
+            </div>
+            <div class="bb col-md my-2 me-2 bg-secondary">
+                <div class="box py-5 mt-1 text-center text-white">
+                    <p>Jumlah Pengajuan yang Disetujui</p>
+                    <span class="display-6 fw-bold"><?= count($disetujui); ?></span>
+                </div>
+            </div>
+            <div class="bb col-md my-2 ms-2 bg-light">
+                <div class="box py-5 mt-1 text-center text-black">
+                    <p>Jumlah Pengajuan yang Tidak Disetujui</p>
+                    <span class="display-6 fw-bold"><?= count($ditolak); ?></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
-<div>
-    <h2>Jumlah Pengajuan yang Disetujui</h2>
-    <h3><?= count($disetujui); ?></h3>
-</div>
-
-<div>
-    <h2>Jumlah Pengajuan yang Tidak Disetujui</h2>
-    <h3><?= count($ditolak); ?></h3>
-</div>
-
-<?= $this->endSection() ;?>
+<?= $this->endSection(); ?>
--- app/Views/layout/dosen.php
+++ app/Views/layout/dosen.php
@@ -8,36 +8,50 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
     <title>Dosen</title>
 </head>
 
 <body class="container">
     <header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="<?= base_url('dosen') ?>">Home</a>
-                </li>
-                <li>
-                    <a href="<?= base_url('dosen/pengajuan') ?>">Pengajuan</a>
-                </li>
-                <li>
-                    <a href="">Panduan</a>
-                </li>
-            </ul>
-
-            <div>
-                <p><?= $nama; ?></p>
-                <a href="<?= base_url('user/logout'); ?>">logout</a>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">BiTA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="<?= base_url('dosen') ?>">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url('dosen/pengajuan') ?>">Pengajuan</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="d-flex">
+                    <div class="dropdown text-end">
+                        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="userMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="<?= base_url('assets/img/profile.svg'); ?>" alt="mdo" width="32" height="32" class="rounded-circle">
+                        </a>
+                        <ul class="dropdown-menu text-small" aria-labelledby="userMenu">
+                            <li><a class="dropdown-item" href="#"><?= session()->get('nama'); ?></a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="<?= base_url('user/logout'); ?>">Logout out</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
-
         <?php if (session()->getFlashData('alert')) {
             echo session()->getFlashData('alert');
         } ?>
-
     </header>
-    <main>
+
+    <main class="my-3">
         <?= $this->renderSection('content'); ?>
     </main>
 
